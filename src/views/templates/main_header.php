698ed2aaf57157c4aafb62da43eba39cd9a089fe
VladMocanu7264 <125999670+VladMocanu7264@users.noreply.github.com>
2024-06-30 23:11:39 +0300

    Upgraded header

--- src/views/templates/main_header.php
+++ src/views/templates/main_header.php
@@ -7,10 +7,17 @@
         </a>
     </div>
     <div class="right-section">
+        <a href="/PawAlert/FePA/src/public/news">News</a>
+        <a href="/PawAlert/FePA/src/public/statistics">Statistics</a>
         <a href="/PawAlert/FePA/src/public/about">About Us</a>
         <a href="/PawAlert/FePA/src/public/help">Help</a>
-        <a href="/PawAlert/FePA/src/public/profile">
+        <div class="dropdown">
             <img src="/PawAlert/FePA/src/public/assets/images/avatar.png" alt="Profile" class="main-header__avatar">
-        </a>
+            <div class="dropdown-content">
+                <a href="/PawAlert/FePA/src/public/profile">Profile</a>
+                <a href="/PawAlert/FePA/src/public/settings">Settings</a>
+                <a href="/PawAlert/FePA/src/public/logout">Log out</a>
+            </div>
+        </div>
     </div>
 </header>
